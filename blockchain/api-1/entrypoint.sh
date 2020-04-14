32b8e5f748de0a7036eeefea67b3e5ac34bf10af
shinneng <shinneng@proximax.io>
2020-04-14 19:14:41 +0800

    api entrypoint with reset condition

--- blockchain/api-1/entrypoint.sh
+++ blockchain/api-1/entrypoint.sh
@@ -11,8 +11,32 @@ if [[ -f "/data/broker.lock" ]]; then
 fi
 
 if [[ -f "/data/recovery.lock" ]]; then
-    echo "unable to recover. Please run reset-node.sh script to reset node"
-    exit 1
+    echo "unable to recover. resetting node."
+    echo "backing up nemesis block"
+    mv /data/00000/00001.dat /data/00000/hashes.dat /tmp
+    echo "deleting data directory"
+
+    for dir in /data/*; do
+        rm -rf $dir
+    done
+
+    if [ -d "/mongodata" ]; then
+        echo "deleting mongodata directory"
+        for dir in /mongodata/*; do
+            rm -rf $dir
+        done
+    fi
+
+    mkdir /data/00000
+    mv /tmp/00001.dat /tmp/hashes.dat /data/00000/
+
+    if [ ! -f "/data/index.dat" ]; then
+        echo "No index.dat file, creating now...."
+        echo -ne "\01\0\0\0\0\0\0\0" > /data/index.dat
+    fi
+    echo "data has been reset to nemesis block"
 fi
+
+sleep 5
 
 /sirius/bin/sirius.bc /chainconfig
